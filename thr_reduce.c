f0faa7e0f294b4d3a039e87a27d73f14d4f242ae
ahmad-jundi <akjundi2020@gmail.com>
2023-02-15 14:50:48 -0500

    thr_reduce.c Commented Final Draft
    This is the commented final draft

--- thr_reduce.c
+++ thr_reduce.c
@@ -31,30 +31,37 @@ void *quad_root_sum(void *thread_temp)
     // Store the local sum in the array of partial sums
     partial_sums[(int)thread_id] = thread_sum;
 
+    //Reducing the partial sums
     for (int i = 1; i < m/2; i++)
     {
-        //when in doubt print it out
+        //Determine the partner thread
         int partner = (int)thread_id ^ (1 << (i-1));
+        //Check if the current thread is responsible for reducing its partners sum
         if (thread_id > partner) {
-            pthread_join(threads[partner], NULL);//Avoids a race condition, in other words, thread_id waits for partner to completely finish
+
+            //Avoids a race condition, in other words, thread_id waits for partner to completely finish
+            pthread_join(threads[partner], NULL);
+
+            //Add partner's sum to current thread's partial sum
             partial_sums[thread_id] += partial_sums[partner];
-            printf("thread_id %ld partner %ld \n", (long)thread_id, (long)partner);
+
+            //Debugging statement to help us, commented out as it is no longer needed
+            //printf("thread_id %ld partner %ld \n", (long)thread_id, (long)partner);
         }
         else {
-            //partial_sums[thread_id] += partial_sums[partner]; Commented this to show Steve/Justin
-            //printf("ELSE thread_id %ld partner %ld \n", (long)thread_id, (long)partner);
+            //Exit loop if current thread is not responsible for reducing its partners sum
             return 0;
         }
     }
-    printf("thread_id %ld \n", (long)thread_id);
+    //Another debugging statement to show if the last active thread is 7, commented it out again
+    //printf("thread_id %ld \n", (long)thread_id);
     for (int i=0; i < m; i++) {
-        printf("thr %d: %f\n", i, partial_sums[i]); //Fix this shit to look nice for tomorrow :) 
+        printf("thr %d: %f\n", i, partial_sums[i]);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    printf("test\n");
     // Get the number of threads and limit of sum calculation from arguments
     m = atoi(argv[1]);
     n = atoi(argv[2]);
@@ -64,10 +71,12 @@ int main(int argc, char *argv[])
 
     // Initializing threads array
     threads = (pthread_t *)malloc(sizeof(pthread_t) * m);
+
         // Create `m` threads
         for (long i = 0; i < m; i++)
     {
-        pthread_create(&threads[i], NULL, quad_root_sum, (void *)i); // void* makes it so no matter what type of value i is, it will take that
+        //Each thread executes quad_root_sum function with 'i' as argument
+        pthread_create(&threads[i], NULL, quad_root_sum, (void *)i);
     }
 
     // Calling pthread_exit() allows other threads to continue execution
